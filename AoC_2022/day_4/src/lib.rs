e1f892eaf0c71cc11d2bf2b6f8f9ea4969525a21
IshanGrover2004 <groverishan2004@gmail.com>
2023-10-09 11:00:37 +0530

    Completed: Day 4

--- AoC_2022/day_4/src/lib.rs
+++ AoC_2022/day_4/src/lib.rs
@@ -48,8 +48,51 @@ pub mod part1 {
 }
 
 pub mod part2 {
-    use std::ops::RangeInclusive;
-    pub fn method1(input: &str) {}
+    use std::ops::{RangeBounds, RangeInclusive};
+
+    trait InclusiveRangeExt {
+        // Check one range contains fully in another range
+        fn overlap(&self, other: &Self) -> bool;
+        fn is_overlaped(&self, other: &Self) -> bool {
+            self.overlap(other) || other.overlap(self)
+        }
+    }
+
+    impl<T> InclusiveRangeExt for RangeInclusive<T>
+    where
+        T: PartialOrd,
+    {
+        fn overlap(&self, other: &Self) -> bool {
+            self.contains(other.start()) || self.contains(other.end())
+        }
+    }
+    pub fn method1(input: &str) -> usize {
+        let result = input
+            .lines()
+            .map(|line| {
+                line.split(",")
+                    .map(|range| {
+                        let mut iter = range
+                            .split("-")
+                            .map(|num| num.parse::<u32>().expect("Failed to parse in u32"));
+                        let start = iter.next().unwrap();
+                        let end = iter.next().unwrap();
+                        start..=end
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .filter(|ranges| {
+                ranges.iter().enumerate().any(|(i, a)| {
+                    ranges
+                        .iter()
+                        .enumerate()
+                        .any(|(j, b)| i != j && a.is_overlaped(b))
+                })
+            })
+            .count();
+
+        result
+    }
 }
 
 #[cfg(test)]
--- AoC_2022/day_4/src/main.rs
+++ AoC_2022/day_4/src/main.rs
@@ -3,6 +3,7 @@ use day_4::{part1, part2};
 fn main() {
     let input = include_str!("input.txt");
 
-    println!("{}", part1::method1(&input));
-    part2::method1(&input);
+    dbg!(part1::method1(&input));
+
+    dbg!(part2::method1(&input));
 }
