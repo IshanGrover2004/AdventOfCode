25162926b1ce8d4e7f89ef45da20f2a2d4d23717
IshanGrover2004 <groverishan2004@gmail.com>
2023-12-01 18:46:12 +0530

    Completed: AoC 2023 - Day 1

--- AoC_2023/day_01/src/lib.rs
+++ AoC_2023/day_01/src/lib.rs
@@ -27,4 +27,43 @@ pub fn solve_part1(input: &str) -> u16 {
         .sum::<u16>()
 }
 
-pub fn solve_part2(input: &str) {}
+fn parse_chars_num(str: &str) -> Vec<u16> {
+    let mut numbers_vec: Vec<u16> = Vec::new();
+    let mut current_number_str = String::new();
+
+    let words: Vec<&str> = vec![
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    for (pos, ch) in str.chars().enumerate() {
+        if ch.is_digit(10) {
+            let val = ch.to_string().parse::<u16>().unwrap();
+
+            numbers_vec.push(val);
+            current_number_str.clear();
+        } else {
+            let s: &str = &str[pos..str.len()];
+            for (idx, word_num) in words.iter().enumerate() {
+                if s.starts_with(word_num) {
+                    numbers_vec.push(idx as u16);
+
+                    break;
+                }
+            }
+        }
+    }
+
+    numbers_vec
+}
+
+pub fn solve_part2(input: &str) -> u16 {
+    let parsed_vec_int = input
+        .lines()
+        .map(|line| parse_chars_num(line))
+        .collect::<Vec<Vec<_>>>();
+
+    parsed_vec_int
+        .iter()
+        .map(|vec| get_calibration_value(vec))
+        .sum::<u16>()
+}
