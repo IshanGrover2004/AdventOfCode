973aefa172e59aa522118e515292bb465ba7b057
IshanGrover2004 <groverishan2004@gmail.com>
2023-12-08 21:51:45 +0530

    feat: Add template for rust code

---
+++ AoC_2023/template_rs/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn solve_part1(input: &str) -> u64 {
+    todo!()
+}
+
+pub fn solve_part2(input: &str) -> u64 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = "";
+        let result = solve_part1(input);
+        assert_eq!(result, 35);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = "";
+        let result = solve_part1(input);
+        assert_eq!(result, 35);
+    }
+}
